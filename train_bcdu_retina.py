1541d0c15eb0802531c2428c4003ff5f01bf4071
MohammedMujahid <mujahid14916@gmail.com>
2020-01-05 14:38:14 +0530

    Updates training script

--- train_bcdu_retina.py
+++ train_bcdu_retina.py
@@ -21,26 +21,17 @@ TOTAL_VAL_DATA_BATCHES = 1000
 WEIGHT_FILE_NAME = 'models/bcdu_weight_lstm.hdf5'
 EPOCHS = 5
 
-val_gen = data_generator('testing_dataset', 'pre-processed', 'label-1', 'png', batch_size=BATCH_SIZE, patch_size=PATCH_SIZE)
-X_val = None
-Y_val = None
-for i in range(TOTAL_VAL_DATA_BATCHES):
-    x, y = next(val_gen)
-    if X_val is None or Y_val is None:
-        X_val, Y_val = x, y
-    else:
-        X_val = np.concatenate([X_val, x], axis=0)
-        Y_val = np.concatenate([Y_val, y], axis=0)
-
 #model = M.unet2_segment(input_size = (64,64,1))
+print("Initializing Network")
 model = M.BCDU_net_D3(input_size = (*PATCH_SIZE, 1))
 if os.path.isfile(WEIGHT_FILE_NAME):
+    print("Loading Saved Model")
     model.load_weights(WEIGHT_FILE_NAME)
 model.summary()
 
 
 # mcp_save = ModelCheckpoint('weight_lstm.hdf5', save_best_only=True, monitor='val_loss', mode='min')
-mcp_save = ModelCheckpoint('models/bcdu_weight-{epoch:02d}-{val_acc:.2f}.hdf5', monitor='val_loss', mode='min')
+mcp_save = ModelCheckpoint('models/bcdu_weight-{epoch:02d}-{val_accuracy:.6f}.hdf5', monitor='val_loss', mode='min')
 reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
 
 history = model.fit_generator(data_generator('training_dataset', 
@@ -50,7 +41,13 @@ history = model.fit_generator(data_generator('training_dataset',
                                              patch_size=PATCH_SIZE),
                               steps_per_epoch=TOTAL_BATCHES,
                               epochs=EPOCHS,
-                              validation_data=[X_val, Y_val],
+                              validation_data=data_generator('testing_dataset', 
+                                                             'pre-processed', 
+                                                             'label-1', 
+                                                             'png', 
+                                                             batch_size=BATCH_SIZE, 
+                                                             patch_size=PATCH_SIZE),
+                              validation_steps=TOTAL_VAL_DATA_BATCHES,
                               callbacks=[mcp_save, reduce_lr_loss])
 
 with open('training_history/bcdu_{}.out'.format(time()), 'wb') as f:
